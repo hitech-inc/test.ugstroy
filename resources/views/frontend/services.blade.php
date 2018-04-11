43c02d0a55806751291350688962dc92dbb1994c
Arman85 <advanced315@gmail.com>
2018-04-11 17:11:50 +0600

    added services layout

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -1,11 +1,118 @@
 @extends('frontend.layouts.master')
+
 @section('content')
-
-<div class="container" style="min-height: 600px;">
-	
-
-
-
+<div class="containerServices">
+	<div class="mainImg">
+		<h3>все по расчету лучших инженеров</h3>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sta
+		ndard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+		type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining 
+		essentially unchanged. It was popularised in the 2000s with the release of Letraset sheets containing Lorem Ipsum</p>
+	</div>
+	<div class="servicesLists">
+		<div class="row" style="margin-top: 0">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service1.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Капитальное строительство зданий</h3>
+					<p>Капитальное строительство предполагает возведение объектов полностью, начиная от фундамента, заканчивая подведением коммуникаций. Компания “ЮГСТРОЙ ИНВЕСТ” имеет обширный опыт в строительстве сложных объектов. У нас работают высококлассные специалисты, профессионалы в своей области. Также, мы владеем обширной материально-технической базой, что помогает в строительстве зданий любой сложности – от небольшого коттеджа до крупного торгового центра</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service2.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Достройка и реконструкция домов</h3>
+					<p>Квалифицированные специалисты “ЮГСТРОЙ ИНВЕСТ” имею огромный опыт реконструкции и достройки домов. Основные конкурентные преимущества нашей компании – это доступные цены, удобные планировочные решения и реализация идей любой сложности. Доверьте реконструкцию своего дома профессионалам.</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service3.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Проектно конструкторские услуги</h3>
+					<p>омпания ТОО “ЮГСТРОЙ ИНВЕСТ” предлагает широкий ассортимент инженерно-конструкторских услуг, направленных на оптимальное строительство сооружений и зданий любой сложности. К таким услугам можно отнести:
+					-Организация и управление проектами;
+					-Менеджмент риска и анализ надежности зданий и сооружений;
+					-Разработка проектной и рабочей конструкторской документации;</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service4.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Экспертно консультационные услуги</h3>
+					<p>Строительный проект – это не только сам непосредственный процесс строительство, но и работа по оформлению многочисленной документации. Именно поэтому, в процессе реализации проекта, крайне нужны профессиональные консультации. Компания ТОО “ЮГСТРОЙ ИНВЕСТ” предлагает все необходимые услуги по технической и юридической </p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service5.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Составление сметной документации</h3>
+					<p>Смета – это документ, в который входят расчеты реальной стоимости услуг и материалов. Без сметы невозможны проведение строительных работ и реализация проектов любого масштаба. Одно из направлений нашей компании – это услуги по составлению сметной документации на различные виды работ, будь то строительство небольших домов, или реализация крупных промышленных, жилых или коммерческих проектов.</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service6.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Производство строительных изделий</h3>
+					<p>Компания ТОО “ЮГСТРОЙ ИНВЕСТ” стремиться максимально решить задачу по обеспечению собственных строительных объектов необходимыми строительными материалами. Мы применяем современную материально-техническую базу для производства строительных материалов, соответствующих высоким мировым стандартам.</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="serviceItem">
+					<img src="img/service7.jpg" alt="" style="max-width: 100%">
+				</div>
+			</div>
+			<div class="col-lg-8">
+				<div class="serviceDesc">
+					<h3>Торгово посредническая деятельность</h3>
+					<p>Компания “ЮГСТРОЙ ИНВЕСТ” работает с лидерами строительной области и предлагает воспользоваться торгово-посредническими услугами на территории ЮКО.
+					Очень часто есть необходимость в приобретении редких и малодоступных материалов, используемых в строительстве. Мы работаем с большим</p>
+					<a href="#">Подробнее</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
 
