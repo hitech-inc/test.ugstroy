be789318488e3b67c112da3ec4d55a78ba505d59
Khodzha Abulkhair <khodzha_au@mai.ru>
2018-04-11 18:44:55 +0600

    add about layout

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,10 +1,34 @@
 @extends('frontend.layouts.master')
 @section('content')
 
-<div class="container" style="min-height: 600px;">
-	
-
-
+<div id="about-block">
+  <h1>Качество проверенное временем</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur nam, omnis perspiciatis reprehenderit temporibus sint porro libero. Id aut nam animi eveniet perferendis repudiandae aperiam, mollitia expedita officia consectetur laboriosam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur nam, omnis perspiciatis reprehenderit temporibus sint porro libero. Id aut nam animi eveniet perferendis repudiandae aperiam, mollitia expedita officia consectetur laboriosam.</p>
+</div>
+<div id="about-text">
+	<div class="text-container">
+		<h3>О компании</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi nihil minima, magni repellat veniam? Ipsum sit provident ducimus dolores quibusdam expedita beatae perferendis temporibus, vero, magnam, at dolorem. Soluta in quibusdam rerum id asperiores quae voluptates! Iure maiores rem fugiat, debitis, nemo quis et perspiciatis animi itaque mollitia ad dicta magnam qui deleniti! Consequatur vitae, velit minus architecto soluta quam eum animi temporibus ullam culpa, quae facere sint ut quod nihil error provident, omnis porro? Omnis voluptatibus, dicta in laboriosam.</p>
+		<p>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam omnis sapiente, culpa quae. Fugiat sed quibusdam eveniet, nostrum molestias vero aut, quis repudiandae ad iusto? Dolores voluptates velit consectetur delectus vel aliquid ratione alias nisi, quis quisquam, reprehenderit nemo laborum reiciendis quo sequi praesentium temporibus distinctio, hic rerum magnam facilis! Dolore molestiae, quam explicabo reprehenderit, veniam accusantium quia, quibusdam fugit officia maiores quos! Amet repellat libero a, soluta maiores exercitationem nemo sit voluptatem nobis tempore. Eveniet neque mollitia at sapiente.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quisquam fuga odio ex perspiciatis debitis maiores architecto dolorem deleniti eos qui cupiditate amet non, temporibus, ut quasi aspernatur! Cum deserunt autem nam recusandae provident aliquid est quas, magnam totam, alias rerum tempore animi, omnis? Et exercitationem vel odit tempora itaque, magnam distinctio harum, totam voluptatibus enim quod. Quia id molestias sed earum voluptatum facere aspernatur ducimus dolorum et perspiciatis at, fuga dolore enim adipisci dignissimos mollitia. Aliquam sed, maxime harum.</p>
+	</div>
+	<div id="worker">
+		<img src="/img/worker.png" alt="">
+	</div>
+	<div id="stats">
+		<div>
+			<p>500</p>
+			<p>Продаются квартир</p>
+		</div>
+		<div>
+			<p>2000</p>
+			<p>Квартир продано</p>
+		</div>
+		<div>
+			<p>350</p>
+			<p>Ожидают</p>
+		</div>
+	</div>
 </div>
 @endsection
 
